c612383f07ce5fe6749b9d716c61105cc0a99675
powalowska <lucyna.powalowska@gmail.com>
2020-04-19 18:45:22 +0200

    express, redis, postgres

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const keys = require('./keys');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+console.log(keys);
+
+// Postgres Client setup
+const { Pool } = require('pg');
+const pgClient = new Pool({
+  user: keys.pgUser,
+  host: keys.pgHost,
+  database: keys.pgDatabase,
+  password: keys.pgPassword,
+  port: keys.pgPort
+})
+
+pgClient.on('error', () => console.log('Lost connection to PG'))
+
+pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)').catch(err => console.log(err));
+
+app.listen(5000, err => {
+  console.log('Backend listening');
+})
